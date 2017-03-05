3e0c4b300e0d23fa66f4edb6d4f037ac9a023a2a
mr-valentos <nerpavlad@gmail.com>
2017-03-05 19:21:38 +0200

    Revert "Add category view"

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import joinedload
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models.category import Category
-from models.food import Food
-
 
 @app.route('/', methods=['GET'])
 def index():
-
-    return render_template('index.html', categories=Category.query.all())
-
-
-@app.route('/category/<id>', methods=['GET'])
-def category(id):
-
-    return render_template('category.html', category=Category.query.get(id))
+    from models.food import Food
+    return render_template('index.html', foods=Food.query.all())
 
 if __name__ == '__main__':
     app.run()
